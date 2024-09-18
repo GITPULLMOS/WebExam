9ec9d4484834725421c34ffba2ce8c38098ec59b
GITPULLMOS <sharifiomran9@gmail.com>
2024-09-18 15:37:03 +0200

    Nav upgrade
    The nav wil dissapear and reappear when scrolling, and the under Menu will follow the top menu. The footer's height is reduced so that when the nav appears the colors wont blend and instead have a gap between the nav and the footer so that the user can easily differentiate them

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 
-
-
-
 let displayUnderMenu = false
 
 function showUnderMenu() {
-    var underMenu = document.getElementById("under-Nav")
+    var underMenu = document.getElementById("bottomNav")
+    var MenuIcon = document.getElementById("menu-Icon")
 
     if (!displayUnderMenu) {
         underMenu.style.display = "flex"
+        MenuIcon.src = "kryss.webp"
         displayUnderMenu = true
     } else {
         underMenu.style.display = "none"
+        MenuIcon.src = "menuIcon.webp"
         displayUnderMenu = false
     }
 }
 
-function showCover() {
-    var Cover = document.getElementById("under-Nav-Cover")
-
-    if (!displayUnderMenu) {
-        Cover.style.display = "flex"
-        displayUnderMenu = true
+var prevScrollpos = window.scrollY
+window.onscroll = function() {
+    var currentScrollpos = window.scrollY
+    if (prevScrollpos > currentScrollpos) {
+        document.getElementById("nav").style.top = "0"
+        document.getElementById("bottomNav").style.display = "none"
+        document.getElementById("menu-Icon").src = "menuIcon.webp"
+        displayUnderMenu = false
     } else {
-        Cover.style.display = "none"
+        document.getElementById("nav").style.top = "-100px"
+        document.getElementById("bottomNav").style.display = "none"
+        document.getElementById("menu-Icon").src = "menuIcon.webp"
         displayUnderMenu = false
     }
+    prevScrollpos = currentScrollpos
 }
