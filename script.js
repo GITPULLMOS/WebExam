8f79d0a3ad4461022ada0642e4eb3e8a1a66e0d3
GITPULLMOS <sharifiomran9@gmail.com>
2024-10-10 15:31:07 +0200

    translating the site

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 
-let displayUnderMenu = false
+// let displayUnderMenu = false
 
-function openUnderMenu() {
-    var underMenu = document.getElementById("bottomNav")
-    var MenuIcon = document.getElementById("menu-Icon")
-    var Cover = document.getElementById("Cover")
+// function openUnderMenu() {
+//     var underMenu = document.getElementById("bottomNav")
+//     var MenuIcon = document.getElementById("menu-Icon")
+//     var Cover = document.getElementById("Cover")
 
-    if (!displayUnderMenu) {
-        underMenu.style.display = "flex"
-        Cover.style.display = "flex"
-        MenuIcon.src = "Images/kryss.webp"
-        displayUnderMenu = true
-    } else {
-        underMenu.style.display = "none"
-        Cover.style.display = "none"
-        MenuIcon.src = "Images/menuIcon.webp"
-        displayUnderMenu = false
-    }
-}
+//     if (!displayUnderMenu) {
+//         underMenu.style.display = "flex"
+//         Cover.style.display = "flex"
+//         MenuIcon.src = "Images/kryss.webp"
+//         displayUnderMenu = true
+//     } else {
+//         underMenu.style.display = "none"
+//         Cover.style.display = "none"
+//         MenuIcon.src = "Images/menuIcon.webp"
+//         displayUnderMenu = false
+//     }
+// }
 
-function closeUnderMenu() {
-    var underMenu = document.getElementById("bottomNav")
-    var MenuIcon = document.getElementById("menu-Icon")
-    var Cover = document.getElementById("Cover")
+// function closeUnderMenu() {
+//     var underMenu = document.getElementById("bottomNav")
+//     var MenuIcon = document.getElementById("menu-Icon")
+//     var Cover = document.getElementById("Cover")
     
-    Cover.style.display = "none"
-    underMenu.style.display = "none"
-    displayUnderMenu = false
-    MenuIcon.src = "Images/menuIcon.webp"
-}
+//     Cover.style.display = "none"
+//     underMenu.style.display = "none"
+//     displayUnderMenu = false
+//     MenuIcon.src = "Images/menuIcon.webp"
+// }
 
-var prevScrollpos = window.scrollY
+// var prevScrollpos = window.scrollY
 
-window.onscroll = function() {
-    var currentScrollpos = window.scrollY
-    if (prevScrollpos < currentScrollpos) {
-        document.getElementById("bottomNav").style.display = "none"
-        document.getElementById("Cover").style.display = "none"
-        document.getElementById("menu-Icon").src = "Images/menuIcon.webp"
-        displayUnderMenu = false
-    }
-    prevScrollpos = currentScrollpos
-}
+// window.onscroll = function() {
+//     var currentScrollpos = window.scrollY
+//     if (prevScrollpos < currentScrollpos) {
+//         document.getElementById("bottomNav").style.display = "none"
+//         document.getElementById("Cover").style.display = "none"
+//         document.getElementById("menu-Icon").src = "Images/menuIcon.webp"
+//         displayUnderMenu = false
+//     }
+//     prevScrollpos = currentScrollpos
+// }
 
 let activeButton = null
 
